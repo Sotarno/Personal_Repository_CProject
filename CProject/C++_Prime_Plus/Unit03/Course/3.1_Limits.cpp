e87d30a9e72dfeef536526dc0582209d7db823d3
Sotarno <jzx.99@163.com>
2023-09-07 22:56:49 +0800

    Update files

--- CProject/C++_Prime_Plus/Unit03/Course/3.1_Limits.cpp
+++ CProject/C++_Prime_Plus/Unit03/Course/3.1_Limits.cpp
@@ -1,4 +1,4 @@
-// llimits.cpp -- some integer limits
+// limits.cpp -- some integer limits
 
 #include <iostream>
 #include <climits>                  // use limits.h for older systems
@@ -12,5 +12,19 @@ int main()
     long long n_llong = LONG_LONG_MAX;
 
     // sezeof operator yields size of type or of variable
-    cout << "int is " << sizeof ()
+    cout << "int is " << sizeof (int) << " bytes." << endl;
+    cout << "short is " << sizeof n_short << " bytes." << endl;
+    cout << "long is " << sizeof n_long << " bytes." << endl;
+    cout << "long long is " << sizeof n_llong << " bytes." << endl;
+    cout << endl;
+
+    cout << "Maxinum values:" << endl;
+    cout << "int: " << n_int << endl;
+    cout << "short: " << n_short << endl;
+    cout << "long: " << n_long << endl;
+    cout << "long long: " << n_llong << endl << endl;
+
+    cout << "Mininum int value = " << INT_MIN << endl;
+    cout << "Bits per byte = " << CHAR_BIT << endl;
+    return 0;
 }
